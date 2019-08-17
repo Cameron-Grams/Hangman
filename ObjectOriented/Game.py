7e6be7cc3cf98c338b0f096f063edc2a529042e1
cameron <cameron.grams.2@gmail.com>
2019-08-17 08:50:55 -0400

    work on the OO model

--- ObjectOriented/Game.py
+++ ObjectOriented/Game.py
@@ -2,6 +2,9 @@ class Game( object ):
     def __init__( self, target_word ):
         self.target_word = target_word
         self.guessed_word = "_" * len( target_word )
+
+        self.target_array = list(target_word)
+        self.guess_array = []
         self.errors = 0
 
     def show_word(self):
@@ -12,3 +15,7 @@ class Game( object ):
     def correct_word ( self ):
         print( self.target_word == self.guessed_word )
         return( self.target_word == self.guessed_word )
+    
+    def guess( self ):
+        guessed_letter = ( input( "Guess a letter: " ) ).upper()
+
--- ObjectOriented/main.py
+++ ObjectOriented/main.py
@@ -2,10 +2,19 @@ from  Game import Game
 import start as st
 import words as w
 
+play_game = False
+
 target_word = st.get_word( w.words )
 
 hangman = Game( target_word )
 
-hangman.show_word()
+like_to_play = input("Would you like to play Hangman?")
+like_to_play = like_to_play.upper()
 
-hangman.correct_word()
+if like_to_play == ("YES" or "Y"):
+    play_game = True
+
+while play_game: 
+    hangman.guess()
+    hangman.show_word()
+    play_game = hangman.correct_word()
